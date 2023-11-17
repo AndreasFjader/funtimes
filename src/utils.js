8add8e7dcd5197c49e6dc0f89cd0c550f6546396
Andreas Fjäder <andreas.fjader@quinyx.com>
2023-11-17 15:07:48 +0100

    missed to save file

--- src/utils.js
+++ src/utils.js
@@ -6,8 +6,8 @@ function manipulateInput(input) {
 
   // It's possible that the selected text was already wrapped in ''.
   // Make sure '' are removed from numerical inputs, and excess '' are removed for strings.
-
   
+
 }
 
 module.exports = { manipulateInput };
