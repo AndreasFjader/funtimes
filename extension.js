508a43ada51388cde45637440870c2a986a66b0a
Andreas Fjäder <andreas.fjader@quinyx.com>
2023-11-24 16:30:41 +0100

    Small rearranging

--- extension.js
+++ extension.js
@@ -3,15 +3,16 @@ const { convertDateTimeToFormat } = require('./src/handlers/datetime-to-format.j
 const { convertDateTimeToEpoch } = require('./src/handlers/datetime-to-epoch.js')
 const { convertDateTimeToIsoUTC } = require('./src/handlers/datetime-to-utc.js')
 const { convertDateTimeToIsoLocal } = require('./src/handlers/datetime-to-local.js')
+
 /**
  * @param {vscode.ExtensionContext} context
  */
-
 async function activate(context) {
 	const convertWithOptionsCommand = vscode.commands.registerCommand('extension.convertWithOption', convertDateTimeToFormat);
 	const convertToEpochCommand = vscode.commands.registerCommand('extension.convertToEpoch', convertDateTimeToEpoch);
 	const convertToUtcCommand = vscode.commands.registerCommand('extension.convertToUtc', convertDateTimeToIsoUTC);
 	const convertToLocalCommand = vscode.commands.registerCommand('extension.convertToLocal', convertDateTimeToIsoLocal);
+
 	context.subscriptions.push(convertWithOptionsCommand);
 	context.subscriptions.push(convertToEpochCommand);
 	context.subscriptions.push(convertToUtcCommand);
